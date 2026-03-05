3cf50f5c658b105dfdd85e12fcd08fd96e0e87e5
DESKTOP-ONJ3R18\Playdata <dldmsrudq@gmail.com>
2026-03-05 09:42:25 +0900

    test

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -12,7 +12,7 @@
             </div>
         </v-col>
         <v-col class="text-center">
-            <v-btn :to="'/'">spring shop</v-btn>
+            <v-btn :to="'/'">vue shop</v-btn>
         </v-col>
         <v-col calss="d-flex justify-end">
             <v-btn v-if="isLogined" :to="'/order/cart'" >장바구니{{ totalQuantity }}</v-btn>
